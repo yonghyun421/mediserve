e9f79955584ef7f62df944e8bc243557711b1e54
71summernight <71summernight@naver.com>
2021-11-10 17:38:05 +0900

    word break

--- src/components/Content/Replys/Replys.js
+++ src/components/Content/Replys/Replys.js
@@ -27,7 +27,7 @@ const Replys = ({ comment_info }) => {
         comment_content: replyInput,
         comment_writer: comment_info.comment_writer
           ? comment_info.comment_writer
-          : '원래는 로그인 유저아이디 들어가야됌 Replys component에서 수정해주기',
+          : '원래는 로그인 유저아이디 들어가야됌',
       },
     ];
     setTotalReplys(replys);
@@ -40,21 +40,25 @@ const Replys = ({ comment_info }) => {
   };
 
   return (
-    <Wrapper>
-      {totalreplys.map(reply => {
-        return (
-          <Reply
-            reply={reply}
-            handleDelete={handleDelete}
-            comment_writer={reply.comment_writer}
-            comment_content={reply.comment_content}
-          />
-        );
-      })}
+    <>
+      <Wrapper>
+        {totalreplys.map(reply => {
+          return (
+            <Reply
+              reply={reply}
+              handleDelete={handleDelete}
+              comment_writer={reply.comment_writer}
+              comment_content={reply.comment_content}
+            />
+          );
+        })}
+      </Wrapper>
       <ReplyInput handleAdd={handleAdd} handleAddByEnter={handleAddByEnter} />
-    </Wrapper>
+    </>
   );
 };
 
 export default Replys;
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-bottom: 30px;
+`;
